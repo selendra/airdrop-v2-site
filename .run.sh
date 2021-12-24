7e904b6dc7690f6d804d5f39296f665dcbae350c
combo <combo@combos-MacBook-Pro.local>
2021-12-24 23:31:51 +0700

    init

--- .run.sh
+++ .run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd api && yarn install > /tmp/yarn.log
+cd api && npm install > /tmp/yarn.log
 cd api && pm2 restart selendra-airdrop-v2 > /tmp/selndra-airdrop-v2.log
+cd interface && npm install -g serve
+cd interface && serve -s build
