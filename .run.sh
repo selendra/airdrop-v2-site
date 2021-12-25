2c3cd6e278f4ec313a1f7b4d5a50b3e3981e1d19
combo <combo@combos-MacBook-Pro.local>
2021-12-25 17:57:04 +0700

    init

--- .run.sh
+++ .run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd api && npm install > /tmp/npm.log
-cd api && pm2 restart selendra-airdrop-v2 > /tmp/selndra-airdrop-v2.log
-cd client && npm install -g serve > /tmp/npm.log
-cd client && serve -s build > /tmp/npm.log
+cd $HOME/api && npm install > /tmp/npm.log
+cd $HOME/api && pm2 restart selendra-airdrop-v2 > /tmp/selndra-airdrop-v2.log
+cd $HOME/client && npm install -g serve > /tmp/npm.log
+cd $HOME/client && serve -s build > /tmp/npm.log
